8f4f70fecd304797866e049f3804dd886e237821
RyanHaberle <Ryan.Haberle@georgebrown.ca>
2019-03-18 13:07:44 -0400

    Add files via upload

---
+++ C#/C#Semester2_Assignment/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_project
+{
+    class Ryan_Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string centreName = "";
+            string city = "";
+            double price = 0;
+            int[] month = new int[12];
+            int[] adults = new int[12];
+            int[] children = new int[12];
+            double[] monthlyRev = new double[12];
+            int DataCentreInfo;
+
+            centreName = "";
+
+            DataCentreInfo = getdata(  price, month, adults, children, monthlyRev, centreName, city);
+            Ryan_centre Cd = new Ryan_centre(price, month, adults, children, monthlyRev, centreName, city);
+            Console.WriteLine(Cd);
+           
+        }
+        public static int getdata(double price, int[] month, int[] adults, int[] children, double[] monthlyRev, string centreName, string city)
+            {
+            Console.WriteLine("How many centres are there to compare?");
+            int numberOfCentres = Convert.ToInt32(Console.ReadLine());
+            int x = 1;
+            int i = 0;
+            for (int count = 0; count < numberOfCentres; x++, count++)
+                {
+                //name input
+                Console.WriteLine("What is the name of centre {0} ", x);  
+                centreName = Console.ReadLine();
+                Console.Clear();
+                //city input
+                Console.WriteLine("What city is the {0} located in?", centreName); 
+                city = Console.ReadLine();
+                Console.Clear();
+                // price input 
+                Console.WriteLine("What is the price of an adult ticket at the {0}?", centreName); 
+                price = Convert.ToDouble(Console.ReadLine());
+                Console.Clear();
+                // numof months input 
+                Console.WriteLine("How many months would you like to compare?");
+                int NumofMonth = Convert.ToInt32(Console.ReadLine());
+                month = new int[NumofMonth];                 //parallel arrays for comparisons.
+                adults = new int[NumofMonth];
+                children = new int[NumofMonth];
+                monthlyRev = new double[NumofMonth];
+
+                for (i = 1; i <= NumofMonth; i++)
+                {
+                    Console.WriteLine("What month would you like to compare? Jan = 1, Dec = 12");
+                    month[i - 1] = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("How many adult admits attended the centre in {0}? ", Ryan_centre.ToDate(month[i - 1]));
+                    adults[i - 1] = Convert.ToInt32(Console.ReadLine());
+                    if (adults[i - 1] < 0)
+                    {
+                        Console.WriteLine("Admits must be zero or more.");
+                        adults[i - 1] = 0;
+                    }
+
+                    Console.WriteLine("How many Child admits attended the center in");
+                    children[i - 1] = Convert.ToInt32(Console.ReadLine());
+                    if (children[i - 1] < 0)
+                    {
+                        Console.WriteLine("Admits must be zero or more.");
+                        adults[i - 1] = 0;
+                        continue;
+                    }
+                    monthlyRev[i - 1] = Ryan_centre.ReturnRev(adults[i - 1], price);
+
+                }
+
+                Console.WriteLine("YEAR 2016");
+                Console.WriteLine("Displaying data for {0}:", centreName);
+                for (i = 0; i < NumofMonth; i++)
+                {
+                    Console.WriteLine("\n Month {0}: {1}", x, Ryan_centre.ToDate(month[i]));
+                    Console.WriteLine("\nNumber of adults: {0}", adults[i]);
+                    Console.WriteLine("\nNumber of children {0}", children[i]);
+                    Console.WriteLine("\nRevenue for {0} is: {1}", Ryan_centre.ToDate(month[i]), monthlyRev[i]);
+
+                }
+
+                int adultHighest = Ryan_centre.GetHighestvisitsOneMonth(adults, month, adults.Length);
+                int childHighest = Ryan_centre.GetHighestvisitsOneMonth(children, month, children.Length);
+                double MonthLowestRev = Ryan_centre.GetLowestMonth(monthlyRev, month, month.Length);
+                double amountOfleastRev = Ryan_centre.GetLowestRevNum(monthlyRev, month, month.Length);
+                string MonthConverted = Ryan_centre.ToDate(MonthLowestRev);
+                string display = "";
+                display.ToString();
+
+                Console.WriteLine("\nDiaplying Statistical Report for 2016:\n");
+                Console.WriteLine("\n\nhighest adult attendance: {0} ", Ryan_centre.ToDate(adultHighest));
+                Console.WriteLine("highest child attendance: {0}", Ryan_centre.ToDate(childHighest));
+                Console.WriteLine("Month of least revenue: {0}", Ryan_centre.ToDate(MonthLowestRev));
+                Console.WriteLine("Amount of revenue in {0}: {1}", MonthConverted, amountOfleastRev);
+
+
+                Console.WriteLine();
+
+                Console.WriteLine("");
+                Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+            }
+                return i;
+            }
+            
+
+        
+    }
+     
+}
+
+
+
+
+
---
+++ C#/C#Semester2_Assignment/Ryan_centre.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_project
+{
+    public class Ryan_centre
+    {
+        private string centreName;
+        private string city;
+        private double price;
+        private int[] month;
+        private int[] adults;
+        private int[] children;
+        
+        public int[] monthCount;
+        
+
+        public static double ReturnRev(double  a, double b)
+        {
+            double total = 0;
+            total = a * b;
+            return total;
+        }
+       
+
+        public static int GetHighestvisitsOneMonth(int[] attendanceNumbers, int[] MothtNumber, int size)
+       {
+            int Month = 0;
+            int highestVists = attendanceNumbers[0];
+            
+            for (int i = 0; i < size; i++)
+            {
+                if (highestVists <= attendanceNumbers[i])
+                {
+                    highestVists = attendanceNumbers[i];
+                    Month = MothtNumber[i];
+                }
+            }
+            return Month;
+
+       }
+        public static double GetLowestMonth(double[] monthlyRev, int[] MothtNumber, int size)
+        {
+            int Month = 0;
+            double LowestMonth = monthlyRev[0];
+
+            for (int i = 0; i < size; i++)
+            {
+                if (LowestMonth >= monthlyRev[i])
+                {
+                    LowestMonth = monthlyRev[i];
+                    Month = MothtNumber[i];
+                }
+            }
+            return Month;
+
+        }
+
+      
+             public static double GetLowestRevNum(double[] monthlyRev, int[] MothtNumber, int size)
+        {
+            double LowestRev = 0;
+            int Month = 0;
+            double LowestMonth = monthlyRev[0];
+
+            for (int i = 0; i < size; i++)
+            {
+                if (LowestMonth >= monthlyRev[i])
+                {
+                    LowestRev = monthlyRev[i];
+                    Month = MothtNumber[i];
+                }
+            }
+            return LowestRev;
+
+        }
+        public Ryan_centre(double Price, int[] Month, int[] Adult, int[] Children, double[] monthlyRev, string centrename, string city)
+        {
+            
+            price = Price;
+            month = new int [Month.Length];
+            adults = new int[Month.Length];
+            children = new int[Month.Length];
+            
+
+        }
+
+        public static string ToDate( double Number )
+        {
+            string[] Darray = new string[12];
+            string MonthConvert = "";
+            string[] dArray =  { "January", "Febuary", " March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+           
+            for (int i = 1; i < dArray.Length+1
+                ; i++)
+            {
+                if (i == Number)
+                {
+                    MonthConvert = dArray[i-1];
+                }
+
+            }
+            return MonthConvert;
+        }
+
+
+        public string CentreName
+        {
+            get
+            {
+                return centreName;
+            }
+            set
+            {
+                centreName = value;
+            }
+        }
+
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+        private double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+        public override string ToString()
+        {
+            return "\tLocation: " + cd;
+                    
+
+        }
+
+
+
+
+
+
+
+    }
+
+            
+
+        }
+    
+
+
+  
+   
+
+         
+
+
